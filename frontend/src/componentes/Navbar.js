66da755adba95844cc0a55d3716f35f809cc178a
NICOLÁS DAVID BECHERMAN <47700068@ort>
2024-10-28 10:13:07 -0300

    menu hamburguesa

--- frontend/src/componentes/Navbar.js
+++ frontend/src/componentes/Navbar.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect, useContext } from 'react';
-import { useNavigate } from 'react-router-dom'; // Importa useNavigate para manejar la navegación
+import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import '../css/Navbar.css';
 import logo from '../img/worky_logo.png';
 import Modal from './modal';
-import Login from './login'; // Importa el componente Login
+import Login from './login';
 import { UserContext } from '../context/UserContext';
 
 const Navbar = () => {
@@ -12,9 +12,8 @@ const Navbar = () => {
   const [categoriasMadre, setCategoriasMadre] = useState([]);
   const [isLoginOpen, setLoginOpen] = useState(false);
   const { token, user, logout } = useContext(UserContext);
-  console.log(token, user)
-
-  const navigate = useNavigate(); // Hook para navegación
+  const [isMenuOpen, setMenuOpen] = useState(false);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchCategoriasMadre = async () => {
@@ -36,16 +35,15 @@ const Navbar = () => {
     setDropdown(null);
   };
 
-  
   const handleLogout = () => {
-    logout()
-    navigate("/")
+    logout();
+    navigate("/");
   };
 
   const handleSearch = async (categoriaNombre) => {
     try {
       const params = { 'CategoriaNombre': categoriaNombre };
-      const response = await axios.get(`http://localhost:5432/Servicio/`, { params });
+      const response = await axios.get('http://localhost:5432/Servicio/', { params });
       const servicios = response.data;
       navigate('/resultados', { state: { searchTerm: categoriaNombre, servicios } });
     } catch (error) {
@@ -67,7 +65,7 @@ const Navbar = () => {
             className="navbar-item"
             onMouseEnter={() => handleMouseEnter(categoria)}
             onMouseLeave={handleMouseLeave}
-            onClick={() => handleSearch(categoria.nombre)} // Llama a handleSearch al hacer clic
+            onClick={() => handleSearch(categoria.nombre)}
           >
             {categoria.nombre}
           </li>
@@ -75,25 +73,36 @@ const Navbar = () => {
       </ul>
       {!token ? (
         <div className="navbar-buttons">
-        <button className="login-btn" onClick={() => setLoginOpen(true)}>Login</button>
-        <a href="/register">
-          <button className="signup-btn">Register</button>
-        </a>
-      </div>
-      ) : (
-        <div>
-        <p>Bienvenido, {user.Nombre}!</p>
-        <button className="signup-btn" onClick={handleLogout}>Cerrar sesión</button>
+          <button className="login-btn" onClick={() => setLoginOpen(true)}>Login</button>
+          <a href="/register">
+            <button className="signup-btn">Register</button>
+          </a>
         </div>
-        )
-    }
+      ) : (
+        <div className="navbar-user">
+          <button className="logout-btn" onClick={handleLogout}>Cerrar sesión</button>
+        </div>
+      )}
+      <div className="hamburger-icon" onClick={() => setMenuOpen(!isMenuOpen)}>
+        <div className="line"></div>
+        <div className="line"></div>
+        <div className="line"></div>
+      </div>
+      <div className={`menu-overlay ${isMenuOpen ? 'open' : ''}`}>
+        <div className="close-menu" onClick={() => setMenuOpen(false)}>&times;</div>
+        <div className="menu-content">
+          <a href="/mi-perfil">Mi perfil</a>
+          <a href="/mis-servicios">Mis servicios</a>
+          <a href="/mis-pagos">Mis pagos</a>
+          <a href="/proximas-citas">Próximas citas</a>
+          <a onClick={handleLogout}>Cerrar sesión</a>
+        </div>
+      </div>
       <Modal isOpen={isLoginOpen} onClose={() => setLoginOpen(false)} title="Log In">
         <Login />
       </Modal>
-
-
     </nav>
   );
-}
+};
 
 export default Navbar;
