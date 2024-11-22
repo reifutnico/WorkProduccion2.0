7874a93f83182eef776d3df938eb16d6651a8354
NICOLÁS DAVID BECHERMAN <47700068@ort>
2024-11-22 11:33:11 -0300

    login&regis

--- frontend/src/componentes/Navbar.js
+++ frontend/src/componentes/Navbar.js
@@ -7,6 +7,7 @@ import Modal from './modal';
 import ModalNotificaciones from './modalNotificaciones';
 import { format } from 'date-fns';
 import Login from './login';
+import Register from './register'; // Importa el componente Register
 import { UserContext } from '../context/UserContext';
 import { FaBell } from 'react-icons/fa';
 import HamburgerMenu from './HamburgerMenu'; // Importa el componente del menú hamburguesa
@@ -16,6 +17,7 @@ const Navbar = () => {
   const [isMenuOpen, setMenuOpen] = useState(false);
   const [categoriasMadre, setCategoriasMadre] = useState([]);
   const [isLoginOpen, setLoginOpen] = useState(false);
+  const [isRegisterOpen, setRegisterOpen] = useState(false); // Nuevo estado para el modal de registro
   const [isNotificationsOpen, setNotificationsOpen] = useState(false);
   const [data, setData] = useState([]);
   const [hasData, setHasData] = useState(false);
@@ -35,50 +37,6 @@ const Navbar = () => {
     fetchCategoriasMadre();
   }, []);
 
-  const fetchData = async () => {
-    try {
-      const response = await axios.get('http://localhost:5000/Servicio/turnoPendiente', {
-        headers: { Authorization: `Bearer ${token}` },
-      });
-      setData(response.data);
-    } catch (error) {
-      console.error('Error al buscar :', error);
-    }
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, [token]);
-
-  useEffect(() => {
-    setHasData(data.length > 0);
-  }, [data]);
-
-  const confirmarTurno = async (nuevoEstado, idTurnoReservado) => {
-    try {
-      const response = await axios.put(
-        `http://localhost:5000/Servicio/turnoPendiente/${idTurnoReservado}`,
-        {},
-        {
-          headers: { Authorization: `Bearer ${token}` },
-          params: { estado: nuevoEstado },
-        }
-      );
-      console.log('Reserva confirmada:', response.data);
-      fetchData();
-    } catch (error) {
-      console.error('Error al confirmar turno:', error);
-    }
-  };
-
-  const handleMouseEnter = (categoriaMadre) => {
-    setDropdown(categoriaMadre.id);
-  };
-
-  const handleMouseLeave = () => {
-    setDropdown(null);
-  };
-
   const handleLogout = () => {
     logout();
     navigate('/');
@@ -86,40 +44,6 @@ const Navbar = () => {
 
   const toggleMenu = () => {
     setMenuOpen(!isMenuOpen);
-  };
-
-  const handleSearch = async (categoriaNombre) => {
-    try {
-      const params = { 'CategoriaNombre': categoriaNombre };
-      const response = await axios.get(`http://localhost:5000/Servicio/`, { params });
-      const servicios = response.data;
-      navigate('/resultados', { state: { searchTerm: categoriaNombre, servicios } });
-    } catch (error) {
-      console.error('Error al buscar servicios:', error);
-    }
-  };
-  const handleMenuOptionClick = (option) => {
-    // Lógica para manejar la opción seleccionada en el menú
-    switch (option) {
-      case 'perfil':
-        navigate('/PerfilUsuario');
-        break;
-      case 'servicios':
-        navigate('/ServiciosContratados');
-        break;
-      case 'mis-servicios':
-        navigate('/ServicesPages ');
-        break;
-      default:
-        break;
-    }
-  };
-  
-  const handleNot = async () => {
-    if (token) {
-      await fetchData();
-      setNotificationsOpen(true);
-    }
   };
 
   return (
@@ -134,9 +58,7 @@ const Navbar = () => {
           <li
             key={categoria.id}
             className="navbar-item"
-            onMouseEnter={() => handleMouseEnter(categoria)}
-            onMouseLeave={handleMouseLeave}
-            onClick={() => handleSearch(categoria.nombre)}
+            onClick={() => navigate(`/search/${categoria.nombre}`)}
           >
             {categoria.nombre}
           </li>
@@ -145,11 +67,8 @@ const Navbar = () => {
       <div className="navbar-actions">
         {token ? (
           <>
-            <div
-              className={`notification-bell ${hasData ? 'red' : ''}`}
-              onClick={handleNot}>
+            <div className="notification-bell" onClick={() => setNotificationsOpen(true)}>
               <FaBell size={24} />
-              {data.length > 0 && <p>{data.length}</p>}
             </div>
             <button className="logout-btn" onClick={handleLogout}>
               Cerrar sesión
@@ -158,9 +77,7 @@ const Navbar = () => {
         ) : (
           <div className="navbar-buttons">
             <button className="login-btn" onClick={() => setLoginOpen(true)}>Login</button>
-            <a href="/register">
-              <button className="signup-btn">Register</button>
-            </a>
+            <button className="signup-btn" onClick={() => setRegisterOpen(true)}>Register</button>
           </div>
         )}
         <div className="hamburger-icon" onClick={toggleMenu}>
@@ -170,64 +87,13 @@ const Navbar = () => {
         </div>
       </div>
 
-      {/* Usando el menú hamburguesa como componente */}
-      <HamburgerMenu
-        isMenuOpen={isMenuOpen}
-        toggleMenu={toggleMenu}
-        handleMenuOptionClick={(option) => handleMenuOptionClick(option)}
-      />
-
-      <Modal isOpen={isLoginOpen} onClose={() => setLoginOpen(false)} title="Log In">
+      <Modal isOpen={isLoginOpen} onClose={() => setLoginOpen(false)} title="Login">
         <Login />
       </Modal>
 
-      <ModalNotificaciones isOpen={isNotificationsOpen} onClose={() => setNotificationsOpen(false)} title="Notificaciones">
-        <div className="notifications-content">
-          {data.length === 0 ? (
-            <p>No tienes turnos reservados.</p>
-          ) : (
-            data.map((turnoReservado, index) => {
-              const fechaObj = new Date(turnoReservado.fecha);
-              fechaObj.setHours(fechaObj.getHours() + 3);
-
-              const comienzoObj = new Date(turnoReservado.comienzo);
-              comienzoObj.setHours(comienzoObj.getHours() + 3);
-
-              const finalObj = new Date(turnoReservado.final);
-              finalObj.setHours(finalObj.getHours() + 3);
-
-              const fecha = format(fechaObj, 'dd/MM/yyyy');
-              const formatHourMinute = (date) => {
-                const hours = String(date.getHours()).padStart(2, '0');
-                const minutes = String(date.getMinutes()).padStart(2, '0');
-                return `${hours}:${minutes}`;
-              };
-              const comienzo = formatHourMinute(comienzoObj);
-              const final = formatHourMinute(finalObj);
-
-              return (
-                <div key={index} className="turno-item">
-                  <h3 className="turno-title">Turno a confirmar</h3>
-                  <div className="modal-items">
-                    <div className="modal-item">
-                      <p><strong>Servicio:</strong> {turnoReservado.Nombre}</p>
-                      <p><strong>Fecha:</strong> {fecha}</p>
-                    </div>
-                    <div className="modal-item">
-                      <p><strong>Comienzo:</strong> {comienzo}</p>
-                      <p><strong>Final:</strong> {final}</p>
-                    </div>
-                    <div className="buttons-container">
-                      <button className="aceptar-btn" onClick={() => confirmarTurno(1, turnoReservado.idTurno)}>Aceptar</button>
-                      <button className="rechazar-btn" onClick={() => confirmarTurno(0, turnoReservado.idTurno)}>Rechazar</button>
-                    </div>
-                  </div>
-                </div>
-              );
-            })
-          )}
-        </div>
-      </ModalNotificaciones>
+      <Modal isOpen={isRegisterOpen} onClose={() => setRegisterOpen(false)} title="Registro">
+        <Register onClose={() => setRegisterOpen(false)} />
+      </Modal>
     </nav>
   );
 };
--- frontend/src/componentes/register.js
+++ frontend/src/componentes/register.js
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import '../css/Login.css';
+import '../css/Login.css'; // Asegúrate de que el archivo CSS esté bien importado
 
 function Register() {
   const [username, setUsername] = useState('');
-  const [email, setEmail] = useState(''); 
-  const [telefono, setTelefono] = useState(''); 
-  const [fechaNacimiento, setFechaNacimiento] = useState(''); 
-  const [fotoPerfil, setFotoPerfil] = useState(''); 
+  const [email, setEmail] = useState('');
+  const [telefono, setTelefono] = useState('');
+  const [fechaNacimiento, setFechaNacimiento] = useState('');
+  const [fotoPerfil, setFotoPerfil] = useState('');
   const [password, setPassword] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
   const navigate = useNavigate();
@@ -19,19 +19,19 @@ function Register() {
       const response = await axios.post('http://localhost:5000/api/account/register', {
         username: username,
         email: email,
-        telefono: telefono, 
-        fechaNacimiento: fechaNacimiento, 
-        fotoPerfil: fotoPerfil, 
+        telefono: telefono,
+        fechaNacimiento: fechaNacimiento,
+        fotoPerfil: fotoPerfil,
         password: password,
       });
 
       if (response.status === 201) {
-        navigate('/confirm'); 
+        navigate('/confirm');
       }
     } catch (error) {
       console.error(error);
       if (error.response && error.response.data && typeof error.response.data.error === 'string') {
-        setErrorMessage(error.response.data.error); 
+        setErrorMessage(error.response.data.error);
       } else {
         setErrorMessage('Error');
       }
@@ -44,56 +44,79 @@ function Register() {
         <h2>Register</h2>
         {errorMessage && <p className="error-message">{errorMessage}</p>}
         <form onSubmit={handleRegister}>
-          <input
-            type="text"
-            placeholder="Nombre"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            required
-          />
-          
-          <input
-            type="email"
-            placeholder="Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)} 
-            required
-          />
-
-          <input
-            type="text"
-            placeholder="Teléfono"
-            value={telefono}
-            onChange={(e) => setTelefono(e.target.value)}
-            required
-          />
-
-          <input
-            type="date"
-            placeholder="Fecha de Nacimiento"
-            value={fechaNacimiento}
-            onChange={(e) => setFechaNacimiento(e.target.value)}
-            required
-          />
-
-          <input
-            type="file"
-            accept="image/*"
-            onChange={(e) => setFotoPerfil(e.target.files[0])} 
-          />
-
-          <input
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-
-          <button type="submit">Register</button>
+          <div>
+            <label htmlFor="username">Nombre</label>
+            <input
+              type="text"
+              id="username"
+              className="form-input"
+              placeholder="Nombre"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              className="form-input"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="telefono">Teléfono</label>
+            <input
+              type="text"
+              id="telefono"
+              className="form-input"
+              placeholder="Teléfono"
+              value={telefono}
+              onChange={(e) => setTelefono(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="fechaNacimiento">Fecha de Nacimiento</label>
+            <input
+              type="date"
+              id="fechaNacimiento"
+              className="form-input"
+              value={fechaNacimiento}
+              onChange={(e) => setFechaNacimiento(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="fotoPerfil">Foto de Perfil</label>
+            <input
+              type="file"
+              id="fotoPerfil"
+              className="form-input"
+              accept="image/*"
+              onChange={(e) => setFotoPerfil(e.target.files[0])}
+            />
+          </div>
+          <div>
+            <label htmlFor="password">Contraseña</label>
+            <input
+              type="password"
+              id="password"
+              className="form-input"
+              placeholder="Contraseña"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          <button type="submit" className="form-button">Registrar</button>
         </form>
         <p>
-          Already have an account? <Link to="/Login">Log in</Link>
+          ¿Ya tienes una cuenta? <Link to="/Login">Inicia sesión</Link>
         </p>
       </div>
     </div>
