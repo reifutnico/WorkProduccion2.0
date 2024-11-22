6486f5c00a5660a28effef43b3e68f26d2548ad7
NICOLAS IAIR REIFUT <47391819@ort>
2024-11-22 11:16:37 -0300

    ...

--- frontend/src/componentes/modalNotificaciones.js
+++ frontend/src/componentes/modalNotificaciones.js
@@ -4,7 +4,7 @@ import '../css/modalNotificaciones.css';
 const ModalNotificaciones = ({ isOpen, onClose, children, title }) => {
     return (
         <div className={`modal-overlay ${isOpen ? 'show' : ''}`} onClick={onClose}>
-            <div className={`modal-content ${isOpen ? 'show' : ''}`} onClick={(e) => e.stopPropagation()}>
+            <div className={`modal-content2  ${isOpen ? 'show' : ''}`} onClick={(e) => e.stopPropagation()}>
                 {children}
             </div>
         </div>
